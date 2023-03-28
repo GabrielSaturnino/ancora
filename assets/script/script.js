f150ab4aa295d5f35792c2757a1faca50c230b00
Gabriel Saturnino <gabrielsaturnino4@gmail.com>
2023-03-28 16:22:08 -0300

    correcao bug
    problema na animação

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,4 +13,8 @@ window.addEventListener('scroll', () => {
   leaf.style.left = value * 1.5 + 'px';
   hill5.style.left = value * 1.5 + 'px';
   hill4.style.left = value * -1.5 + 'px';
+
+
+  if (value <= 700) hill1.style.top = value * 1 + 'px';
+  else hill1.style.top = '0px';
 });
